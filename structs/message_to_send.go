c91e1b668af2a56f6fd0fdb4486e2c88ca605d84
Anthony Kim <fbssaejun@gmail.com>
2024-03-03 19:21:17 -0500

    create user struct and a function to validate structs

--- structs/message_to_send.go
+++ structs/message_to_send.go
@@ -5,13 +5,48 @@ import "fmt"
 type messageToSend struct {
 	phoneNumber int
 	message     string
+	recipient   user
+	sender      user
+}
+
+type user struct {
+	name   string
+	number int
+}
+
+func canSendMessage(mToSend messageToSend) bool {
+	if mToSend.sender.name == "" {
+		return false
+	}
+	if mToSend.recipient.name == "" {
+		return false
+	}
+	if mToSend.sender.number == 0 {
+		return false
+	}
+	if mToSend.recipient.number == 0 {
+		return false
+	}
+
+	return true
 }
 
 func main() {
-	messegeStruct := messageToSend{
+	validMessegeStruct := messageToSend{
 		phoneNumber: 1234567890,
 		message:     "Hello, World!",
+		recipient:   user{"John Doe", 1234567890},
+		sender:      user{"Jane Doe", 1110001111},
 	}
 
-	fmt.Println(messegeStruct.phoneNumber, messegeStruct.message)
+	invalidMessegeStruct := messageToSend{
+		phoneNumber: 1234567890,
+		message:     "Hello, World!",
+		recipient:   user{"", 1234567890},
+		sender:      user{"Jane Doe", 0},
+	}
+
+	fmt.Println(validMessegeStruct.phoneNumber, validMessegeStruct.message)
+	fmt.Println(canSendMessage(validMessegeStruct))
+	fmt.Println(canSendMessage(invalidMessegeStruct))
 }
