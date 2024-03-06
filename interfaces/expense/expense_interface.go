0768da8f301b8810f65249fe2271b7b43c975be9
Anthony Kim <fbssaejun@gmail.com>
2024-03-06 00:13:04 -0500

    create expense interface with getExpenseReport that checks for struct types by trying to cast them to the interface

---
+++ interfaces/expense/expense_interface.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+func getExpenseReport(e expense) (string, float64) {
+	email, ok := e.(email)
+	if ok {
+		return email.toAddress, email.cost()
+	}
+	sms, ok := e.(sms)
+	if ok {
+		return sms.toPhoneNumber, sms.cost()
+	}
+	return "", 0.0
+}
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
+
+func main() {
+	email1 := email{
+		isSubscribed: true,
+		body:         "We've been reaching out to you about your car's extended warranty",
+		toAddress:    "Toronto, ON",
+	}
+	sms1 := sms{
+		isSubscribed:  false,
+		body:          "We've been reaching out to you about your car's extended warranty",
+		toPhoneNumber: "555-555-5555",
+	}
+
+	invalid1 := invalid{}
+
+	fmt.Println(getExpenseReport(email1))
+	fmt.Println(getExpenseReport(sms1))
+	fmt.Println(getExpenseReport(invalid1))
+}
