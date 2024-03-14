629085e032aa08362048eedcdad52b51b898f45c
Anthony Kim <fbssaejun@gmail.com>
2024-03-14 00:49:29 -0400

    add function that omits the looping condition

--- loops/bulk_send.go
+++ loops/bulk_send.go
@@ -13,7 +13,20 @@ func bulkSend(numMessages int) float64 {
 	return totalCost
 }
 
+func maxMessages(thresh float64) int {
+	totalCost := 0.0
+	for i := 0; ; i++ {
+		totalCost += 1 + (0.01 * float64(i))
+		if totalCost > thresh {
+			return i
+		}
+	}
+}
+
 func main() {
 	numMessages := 10
 	fmt.Printf("Cost to send %d messages: $%.2f\n", numMessages, bulkSend(numMessages))
+
+	threshold := 20.0
+	fmt.Printf("Max messages for $%.2f: %d\n", threshold, maxMessages(threshold))
 }
