758a83a52030a1b4100b0b321f6d5689d19ece08
Anthony Kim <fbssaejun@gmail.com>
2024-03-20 22:51:35 -0400

    refactor function to not return any values

--- maps/get_counts.go
+++ maps/get_counts.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func getCounts(messagedUsers []string, validUsers map[string]int) map[string]int {
+func getCounts(messagedUsers []string, validUsers map[string]int) {
 	for _, messagedUser := range messagedUsers {
 		if _, ok := validUsers[messagedUser]; ok {
 			validUsers[messagedUser]++
 		}
 	}
-	return validUsers
+	fmt.Println(validUsers)
 }
 
 func main() {
@@ -19,5 +19,5 @@ func main() {
 	}
 	messagedUsers := []string{"John", "Jane", "Jane"}
 
-	fmt.Println(getCounts(messagedUsers, validUsers))
+	getCounts(messagedUsers, validUsers)
 }
