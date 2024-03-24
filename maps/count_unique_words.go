05730cbfe16242ea80332813eb41372e5637efac
Anthony Kim <fbssaejun@gmail.com>
2024-03-23 21:51:38 -0400

    add one more way of counting unique words

--- maps/count_unique_words.go
+++ maps/count_unique_words.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// 1st solution
 func countUniqueWords(messages []string) int {
 	uniqueWords := make(map[string]bool)
 	for _, message := range messages {
@@ -16,11 +17,24 @@ func countUniqueWords(messages []string) int {
 	return len(uniqueWords)
 }
 
+// 2nd solution
+func countUniqueWordsSecond(messages []string) int {
+	uniqueWords := make(map[string]int)
+	for _, message := range messages {
+		words := strings.Fields(message)
+		for _, word := range words {
+			uniqueWords[word]++
+		}
+	}
+	return len(uniqueWords)
+}
+
 func main() {
 	messages := []string{
 		"Hello, world!",
 		"Hello, earth!",
 		"Bye, world!",
 	}
-	fmt.Println(countUniqueWords(messages)) // 4
+	fmt.Println(countUniqueWords(messages))       // 4
+	fmt.Println(countUniqueWordsSecond(messages)) // 4
 }
