3bad84ff25023cba1c8442c19fa9930ad78f26e2
Anthony Kim <fbssaejun@gmail.com>
2024-03-23 22:52:58 -0400

    add comment

--- functions/advanced_functions/sum.go
+++ functions/advanced_functions/sum.go
@@ -12,6 +12,7 @@ func adder() func(int) int {
 
 func main() {
 	add := adder()
+	// it will add 1 to the "sum" variable defined in the adder() function
 	fmt.Println(add(1)) // 1
 	fmt.Println(add(2)) // 3
 	fmt.Println(add(3)) // 6
